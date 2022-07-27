f3e196cb3eebda960654a5ea08084b1809f9ff81
adamdgit <adam_d@outlook.com>
2022-07-28 02:39:59 +1000

    added get users playlists page

--- src/components/pages/playlists.js
+++ src/components/pages/playlists.js
@@ -1,10 +1,73 @@
+import { useEffect, useContext, useState, useRef } from "react"
+import axios from "axios"
+import { LoginStatusCtx } from "../login";
 
 export default function Playlists() {
+
+  const { token, setToken } = useContext(LoginStatusCtx)
+  const { playerData, setPlayerData } = useContext(LoginStatusCtx)
+  const { playlistData, setPlaylistData } = useContext(LoginStatusCtx)
+
+  const API_PLAYLISTS = "https://api.spotify.com/v1/me/playlists"
+
+  const [playlists, setPlaylists] = useState([])
+  
+  async function playPlaylist(playlist){
+    // set selected playlist data to send to media player
+    setPlayerData({
+      uris: [playlist.uri],
+      play: true,
+      offset: 0
+    })
+    // save currently playing playlist data to global context
+    setPlaylistData({
+      playlist_id: playlist.id,
+      playlist_name: playlist.name,
+      playlist_uri: playlist.uri,
+      playlist_image: playlist.images[0].url,
+      playlist_tracks_href: playlist.tracks.href,
+      playlist_desc: playlist.description
+    })
+  }
+
+  useEffect(() => {
+
+    const getPlaylists = async () => {
+        await axios.get(API_PLAYLISTS, {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          }
+        }).then((res) => {
+          console.log(res.data.items)
+          setPlaylists(res.data.items)
+        }).catch(error => console.error(error))
+    }
+    getPlaylists()
+
+  },[])
 
   return (
     <div className="page-wrap">
       <div className="main-content">
         <h1 className="featured">My Playlists</h1>
+        {
+        playlists.length != 0 ?
+        playlists.map((result, i) => {
+          return (
+            <div key={i} className={'explore-result'}>
+              <img src={result.images[0].url} width={'200px'} height={'200px'} />
+              <h2>{result.name}</h2>
+              <p>{result.description}</p>
+              <button className="play" onClick={() => playPlaylist(result)}>
+                <svg viewBox="0 0 16 16" height="25" width="25" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
+              </button>
+            </div>
+          )
+        })
+        : <></>
+      }
       </div>
     </div>
   )
