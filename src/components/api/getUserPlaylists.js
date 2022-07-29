0b2f5b99c7f62174dd1a599a35ddbf6a3ad5c9bc
adamdgit <adam_d@outlook.com>
2022-07-29 15:48:13 +1000

    Cleaned up user playlists section and we now save playlist data

--- src/components/api/getUserPlaylists.js
+++ src/components/api/getUserPlaylists.js
@@ -1,67 +1,67 @@
-import { useState, useRef } from "react";
-import axios from "axios";
+import { useEffect, useContext, useState, useRef } from "react"
+import axios from "axios"
+import { LoginStatusCtx } from "../login";
 
-export default function GetUserPlaylists({...props}) {
+export default function GetUserPlaylists() {
 
-  const API_URL = `https://api.spotify.com/v1/me/playlists`
+  const { token, setToken } = useContext(LoginStatusCtx)
+  const { playlistData, setPlaylistData } = useContext(LoginStatusCtx)
 
-  const [results, setResults] = useState([])
-  const trackElement = useRef([])
- 
-  const getPlaylists = async () => {
-    try {
-      const {data} = await axios.get(API_URL, {
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Bearer ${props.token}`,
-          'Content-Type': 'application/json',
-        }
-      })
-      console.log(data.items)      
-      return setResults(data.items)
-    } catch (error) {
-      console.error(error)
-      return setResults([])
-    }
+  const [playlists, setPlaylists] = useState([])
+  
+  function playPlaylist(playlist){
+    console.log(playlist)
+    // save currently playing playlist data to global context
+    setPlaylistData({
+      uris: [playlist.uri],
+      play: true,
+      autoplay: true,
+      offset: 0,
+      playlist_id: playlist.id,
+      playlist_name: playlist.name,
+      playlist_uri: playlist.uri,
+      playlist_image: playlist.images[0].url,
+      playlist_tracks_href: playlist.tracks.href,
+      playlist_desc: playlist.description
+    })
   }
 
-  async function playPlaylist(id) {
-    try {
-      await axios.put(`https://api.spotify.com/v1/me/player/play`, {
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Bearer ${props.token}`,
-          'Content-Type': 'application/json',
-        },
-        data: {
-          'context-uri': `spotify:playlist:${id}`,
-        },
-        "position_ms" : 0
-      })   
-    } catch (error) {
-      console.error(error)
+  useEffect(() => {
+
+    const getPlaylists = async () => {
+        await axios.get('https://api.spotify.com/v1/me/playlists', {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          }
+        }).then((res) => {
+          console.log(res.data.items)
+          setPlaylists(res.data.items)
+        }).catch(error => console.error(error))
     }
-  }
+    getPlaylists()
+
+  },[])
 
   return (
-    <div className="searchResults">
-        <button onClick={getPlaylists}>Get my playlists</button>
-      <h1>My Playlists:</h1>
-      {
-        results.length != 0 ?
-        results.map((result, i) => {
+      <>
+        {
+        playlists.length != 0 ?
+        playlists.map((result, i) => {
           return (
-          <div key={i} ref={trackElement[i]} className={'search-result'}>
-            <img src={result.images[0].url} width={'200px'} height={'200px'} />
-            <h1>{result.name}</h1>
-            <h2>{result.description}</h2>
-            <button onClick={() => playPlaylist(result.id)}>Play</button>
-          </div>
+            <div key={i} className={'explore-result'}>
+              <img src={result.images[0].url} width={'200px'} height={'200px'} />
+              <h2>{result.name}</h2>
+              <p>{result.description}</p>
+              <button className="play" onClick={() => playPlaylist(result)}>
+                <svg viewBox="0 0 16 16" height="25" width="25" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
+              </button>
+            </div>
           )
         })
         : <></>
       }
-    </div>
+    </>
   )
-
 }
