cedc6c10de2645bccfc4ed4649df7221a7e53972
adamdgit <adam_d@outlook.com>
2022-08-03 19:47:06 +1000

    Updated global context and changed data sent to player

--- src/components/controls.js
+++ src/components/controls.js
@@ -4,10 +4,11 @@ import { LoginStatusCtx } from "./login";
 import SpotifyWebPlayer from "react-spotify-web-playback/lib";
 
 function MusicPlayer() {
-  
+ 
   // global context
-  const { token, setToken } = useContext(LoginStatusCtx)
-  const { playlistData, setPlaylistData } = useContext(LoginStatusCtx)
+  const { token } = useContext(LoginStatusCtx)
+  const { playerURIS, setPlayerURIS } = useContext(LoginStatusCtx)
+  const { playerOffset, setPlayerOffset } = useContext(LoginStatusCtx)
   const { playerCBData, setPlayerCBData } = useContext(LoginStatusCtx)
   // playlist hidden state
   const [playerIsHidden, setPLayerIsHidden] = useState(true)
@@ -16,7 +17,7 @@ function MusicPlayer() {
     <>
     <Playlist playerIsHidden={playerIsHidden} />
     <div className="controls">
-      {playlistData.uris.length != 0 ? 
+      {playerURIS ?
       <SpotifyWebPlayer
         token={token}
         callback={state => setPlayerCBData({
@@ -27,8 +28,8 @@ function MusicPlayer() {
           track_id: state.track.id,
           type: state.type
         })}
-        uris={playlistData.uris}
-        offset={playlistData.offset}
+        uris={playerURIS}
+        offset={playerOffset}
         styles={{
           bgColor:'#111',
           color:'#fff',
@@ -37,12 +38,12 @@ function MusicPlayer() {
           trackNameColor: '#ccc'
         }}
         initialVolume={.2}
-        play={playlistData.play}
-        autoPlay={playlistData.autoPlay}
+        play={true}
+        autoPlay={true}
       />
       :<></>}
       <button className={playerIsHidden === true ? 'show-hide-playlist-btn' : 'show-hide-playlist-btn rotate-btn'} onClick={() => setPLayerIsHidden(!playerIsHidden)} >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="20px" fill="currentcolor">/*! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.*/<path d="M9.39 265.4l127.1-128C143.6 131.1 151.8 128 160 128s16.38 3.125 22.63 9.375l127.1 128c9.156 9.156 11.9 22.91 6.943 34.88S300.9 320 287.1 320H32.01c-12.94 0-24.62-7.781-29.58-19.75S.2333 274.5 9.39 265.4z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="20px" fill="currentcolor">{/*! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.*/}<path d="M9.39 265.4l127.1-128C143.6 131.1 151.8 128 160 128s16.38 3.125 22.63 9.375l127.1 128c9.156 9.156 11.9 22.91 6.943 34.88S300.9 320 287.1 320H32.01c-12.94 0-24.62-7.781-29.58-19.75S.2333 274.5 9.39 265.4z"/></svg>
       </button>
     </div>
     </>
