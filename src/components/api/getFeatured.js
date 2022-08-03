9a01e531fd50b18032e8e84703f2245c9abe9959
adamdgit <adam_d@outlook.com>
2022-08-03 19:51:03 +1000

    Changed global context variables

--- src/components/api/getFeatured.js
+++ src/components/api/getFeatured.js
@@ -4,64 +4,44 @@ import { LoginStatusCtx } from "../login";
 
 export default function GetFeatured() {
 
-  const { token, setToken } = useContext(LoginStatusCtx)
-  const { playlistData, setPlaylistData } = useContext(LoginStatusCtx)
+  const { token } = useContext(LoginStatusCtx)
+  const { playerURIS, setPlayerURIS } = useContext(LoginStatusCtx)
+  const { playerOffset, setPlayerOffset } = useContext(LoginStatusCtx)
+  const { playlistID, setPlaylistID } = useContext(LoginStatusCtx)
 
   const [results, setResults] = useState([])
   const trackElement = useRef([])
 
   function playPlaylist(playlist){
-    console.log(playlist)
     // save currently playing playlist data to global context
-    setPlaylistData({
-      uris: [playlist.uri],
-      play: true,
-      autoplay: true,
-      offset: 0,
-      playlist_id: playlist.id,
-      playlist_name: playlist.name,
-      playlist_uri: playlist.uri,
-      playlist_image: playlist.images[0].url,
-      playlist_tracks_href: playlist.tracks.href,
-      playlist_desc: playlist.description
-    })
+    setPlayerURIS(playlist.uri)
+    setPlaylistID(playlist.id)
+    setPlayerOffset(0)
   }
-
-  const getFeaturedPlaylists = async () => {
-    try {
-      const {data} = await axios.get(`https://api.spotify.com/v1/browse/featured-playlists`, {
+ 
+  useEffect(() => {
+    const getFeaturedPlaylists = async () => {
+      await axios.get('https://api.spotify.com/v1/browse/featured-playlists', {
         headers: {
           Accept: 'application/json',
           Authorization: `Bearer ${token}`,
           'Content-Type': 'application/json',
         }
-      })
-      console.log(data.playlists.items)
-      return setResults(data.playlists.items)
-    } catch (error) {
-      console.error(error)
+      }).then((res) => {
+        setResults(res.data.playlists.items)
+      }).catch(error => console.error(error))
     }
-  }
- 
-  useEffect(() => {
-    if (token) {
-      console.log('test')
-      getFeaturedPlaylists()
-    }
-    return () => {
-      console.log('cleanup')
-      setResults([])
-    }
+    getFeaturedPlaylists()
   },[token])
 
   return (
     <div className="searchResults">
       {
-        results.length != 0 ?
+        results.length !== 0 ?
         results.map((result, i) => {
           return (
             <div key={i} ref={trackElement[i]} className={'explore-result'}>
-              <img src={result.images[0].url} width={'200px'} height={'200px'} />
+              <img src={result.images[0].url} alt={result.name + 'playlist art'} width={'200px'} height={'200px'} />
               <h2>{result.name}</h2>
               <p>{result.description}</p>
               <button className="play" onClick={() => playPlaylist(result)}>
