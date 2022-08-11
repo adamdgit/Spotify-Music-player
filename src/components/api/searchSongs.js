fa44b97079d62cd71ee84e6990003f220c2d937a
adamdgit <adam_d@outlook.com>
2022-08-11 17:17:27 +1000

    Moved show hide function to func folder for reuse

--- src/components/api/searchSongs.js
+++ src/components/api/searchSongs.js
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect, useContext } from "react";
 import axios from "axios";
 import { LoginStatusCtx } from "../login";
+import { showHideAddToPlaylistBtn } from "../func/showHideAddToPlaylistBtn"
 
 export default function SearchSongs() {
   
@@ -46,20 +47,8 @@ export default function SearchSongs() {
     inputElement.current.value = ''
   }
 
-  function showHideAddToPlaylist(e) {
-    if(!e.classList.contains('add-to-playlist')) return
-    // if add to playlist button is clicked but a different buttons hidden
-    // element is showing, hide it
-    if(e.classList.contains('add-to-playlist') && document.querySelector('.show-p') && !e.querySelector('.show-p')) {
-      document.querySelector('.show-p').classList.remove('show-p')
-    }
-    // if hidden element is shown, hide it
-    if(e.querySelector('.show-p')) {
-      e.querySelector('.show-p').classList.remove('show-p')
-    //if hidden element is hidden, show it
-    } else {
-      e.querySelector('.choose-playlist').classList.add('show-p')
-    }
+  const showBtn = (e) => {
+    showHideAddToPlaylistBtn(e)
   }
 
   function playSong(song) {
@@ -145,7 +134,7 @@ export default function SearchSongs() {
                   return `${artist.name}, `
                 })}
               </span>
-              <button className="add-to-playlist" onClick={(e) => showHideAddToPlaylist(e.target)}>
+              <button className="add-to-playlist" onClick={(e) => showBtn(e.target)}>
                 <svg style={{pointerEvents:"none"}} xmlns="http://www.w3.org/2000/svg" fill="currentcolor" width="20px" viewBox="0 0 512 512">{/* Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg>
                 <span className={"choose-playlist"}>
                   <h3>Add to playlist:</h3>
