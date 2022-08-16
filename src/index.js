f952cca2ec4f66bab28068c1d2e7412626513ceb
adamdgit <adam_d@outlook.com>
2022-08-16 17:32:20 +1000

    removed home.css unused stylesheet

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './styles/header.css';
 import './styles/controls.css'
 import './styles/playlist.css'
 import './styles/explore.css'
-import './styles/home.css'
 import './styles/mobile.css'
+import './styles/userPlaylists.css'
 import Login from './components/login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
