8dbe0ee74a3a0467ed4f9674fdeddd2b7ccce6aa
adamdgit <adam_d@outlook.com>
2022-08-18 03:07:19 +1000

    Added API call for searching songs

---
+++ src/components/api/search.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+let errorMsg
+let result
+
+export async function searchSongs(token, query) {
+  await axios.get('https://api.spotify.com/v1/search', {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    params: {
+      q: query,
+      type: 'track'
+    }
+  }).then((res) => {
+    result = res.data
+  }).catch(error => console.error(error))
+
+  if(errorMsg) return errorMsg
+  return result
+}
