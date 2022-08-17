a7190acc9f100bb097837c2124a736d836bbfca3
adamdgit <adam_d@outlook.com>
2022-08-18 03:08:43 +1000

    Set default playlist name and description

--- src/components/api/createPlaylist.js
+++ src/components/api/createPlaylist.js
@@ -2,18 +2,19 @@ import axios from "axios"
 
 let errorMsg
 
-export async function changePlaylistSong(token, userID, playlistName, playlistDesc, publicPlaylist){
+export async function createPlaylist(token, userID){
   await axios({ 
     method: 'post', 
     url: `https://api.spotify.com/v1/users/${userID}/playlists`, 
     headers: { 'Authorization': 'Bearer ' + token }, 
     data: {
-      "name": playlistName,
-      "description": playlistDesc,
-      "public" : publicPlaylist
+      "name": '#New playlist',
+      "description": 'add description',
+      "public" : false
     }
-  }).catch(error => errorMsg = error)
+  }).catch(error => {
+    errorMsg = error
+  })
   
   if(errorMsg) return errorMsg
 }
-
