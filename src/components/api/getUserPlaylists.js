f372f6b3019b60862b8f69d5bbfb422ff2aafa44
adamdgit <adam_d@outlook.com>
2022-08-18 03:09:49 +1000

    Created edit playlist button in user playlists page

--- src/components/api/getUserPlaylists.js
+++ src/components/api/getUserPlaylists.js
@@ -2,6 +2,7 @@ import { useEffect, useContext, useState } from "react"
 import axios from "axios"
 import { LoginStatusCtx } from "../login";
 import { changePlaylistSong } from "./changePlaylistSong";
+import { NavLink } from "react-router-dom"
 
 export default function GetUserPlaylists() {
 
@@ -42,10 +43,13 @@ export default function GetUserPlaylists() {
         playlists.length !== 0 ?
         playlists.map((result, i) => {
           return (
-            <div key={i} className={'explore-result'}>
+            <div key={i} className={'my-playlists-result'}>
               <img src={result.images.length !== 0 ? result.images[0].url : ''} alt={result.name + 'playlist art'} width={'200px'} height={'200px'} />
               <h2>{result.name}</h2>
               <p>{result.description}</p>
+              <NavLink to={`/editPlaylist/${result.id}`}>
+                <button className="play edit">Edit</button>
+              </NavLink>
               <button className="play" onClick={() => playPlaylist(result)}>
                 <svg viewBox="0 0 16 16" height="25" width="25" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
               </button>
