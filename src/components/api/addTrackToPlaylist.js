add573901f8062bca9cf559f96d751d1e6ca94b8
adamdgit <adam_d@outlook.com>
2022-08-20 02:54:13 +1000

    Get new playlist update after adding track to playlist

--- src/components/api/addTrackToPlaylist.js
+++ src/components/api/addTrackToPlaylist.js
@@ -1,16 +1,29 @@
 import axios from "axios"
 
+let songs = []
 let errorMsg
 
-export async function addTrackToPlaylist(resultURI, playlistid, token) {
+export async function addTrackToPlaylist(resultURI, playlistID, token) {
   await axios({ 
     method: 'post', 
-    url: `https://api.spotify.com/v1/playlists/${playlistid}/tracks`, 
+    url: `https://api.spotify.com/v1/playlists/${playlistID}/tracks`, 
     headers: { 'Authorization': 'Bearer ' + token }, 
     data: {
       "uris": [`${resultURI}`]
     }
   }).catch(error => errorMsg = error)
 
+  // get newly updated playlist items
+  await axios.get(`https://api.spotify.com/v1/playlists/${playlistID}/tracks?limit=50`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    }
+  }).then((res) => {
+    songs = res.data.items
+  }).catch(error => errorMsg = error)
+
   if(errorMsg) return errorMsg
+  return songs
 }
