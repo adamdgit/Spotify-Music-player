5a55bc7f280dd504d6dae810be4552d62262bf4e
adamdgit <adam_d@outlook.com>
2022-08-20 16:17:14 +1000

    Moved getfeatured component to explore page

--- src/components/pages/explore.js
+++ src/components/pages/explore.js
@@ -1,12 +1,62 @@
-import GetFeatured from '../api/getFeatured'
+import { useState, useRef, useContext, useEffect } from "react";
+import { LoginStatusCtx } from "../login";
+import axios from "axios";
 
 export default function Explore() {
+
+  const { token } = useContext(LoginStatusCtx)
+  const { playerURIS, setPlayerURIS } = useContext(LoginStatusCtx)
+  const { playerOffset, setPlayerOffset } = useContext(LoginStatusCtx)
+  const { playlistID, setPlaylistID } = useContext(LoginStatusCtx)
+
+  const [results, setResults] = useState([])
+  const trackElement = useRef([])
+
+  function playPlaylist(playlist){
+    // save currently playing playlist data to global context
+    setPlayerURIS(playlist.uri)
+    setPlaylistID(playlist.id)
+    setPlayerOffset(0)
+  }
+ 
+  useEffect(() => {
+    const getFeaturedPlaylists = async () => {
+      await axios.get('https://api.spotify.com/v1/browse/featured-playlists', {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        }
+      }).then((res) => {
+        console.log(res.data)
+        setResults(res.data.playlists.items)
+      }).catch(error => console.error(error))
+    }
+    getFeaturedPlaylists()
+  },[token])
 
   return (
     <div className="page-wrap">
       <div className="main-content">
         <h1 className="featured">Spotify featured playlists:</h1>
-          <GetFeatured />
+        <div className="searchResults">
+          {
+            results.length !== 0 ?
+            results.map((result, i) => {
+              return (
+                <div key={i} ref={trackElement[i]} className={'explore-result'}>
+                  <img src={result.images[0].url} alt={result.name + 'playlist art'} width={'200px'} height={'200px'} />
+                  <h2>{result.name}</h2>
+                  <p>{result.description}</p>
+                  <button className="play" onClick={() => playPlaylist(result)}>
+                    <svg viewBox="0 0 16 16" height="25" width="25" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
+                  </button>
+                </div>
+              )
+            })
+            : <></>
+          }
+        </div>
       </div>
     </div>
   )
