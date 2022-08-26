2dd012b5606d5d080149a1cb3aee9beeea0162e9
adamdgit <adam_d@outlook.com>
2022-08-26 16:38:16 +1000

    Prevent modal from showing outside of screen view

--- src/components/func/showHideAddToPlaylistBtn.js
+++ src/components/func/showHideAddToPlaylistBtn.js
@@ -13,4 +13,10 @@ export function showHideAddToPlaylistBtn(e) {
   } else {
     e.querySelector('.choose-playlist').classList.add('show-p')
   }
+  // if modal appears below the screen, change its position so user can see it
+  // +100 is a magic number, accounting for padding and margins as javascript
+  // does not have a built in way to get an elements height including those properties
+  if(e.querySelector('.choose-playlist').getBoundingClientRect().bottom > (document.querySelector('.playlist').offsetHeight + 100)) {
+    e.querySelector('.choose-playlist').style.top = '-355px'
+  }
 }
