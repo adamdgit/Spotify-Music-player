04d2212c5958bf97aac0784db36d3e0be45a9d03
adamdgit <adam_d@outlook.com>
2022-08-26 16:35:34 +1000

    Major cleanup of drag & drop function

--- src/components/playlistInfo.js
+++ src/components/playlistInfo.js
@@ -10,8 +10,10 @@ import { addTrackToPlaylist } from "./api/addTrackToPlaylist"
 import { showHideAddToPlaylistBtn } from "./func/showHideAddToPlaylistBtn"
 import { convertTime } from "./func/convertTime"
 import { sanitizeArtistNames } from "./func/sanitizeArtistNames"
+import { getNearestNode } from "./func/getNearestNode"
+import { getUserPlaylists } from "./api/getUserPlaylists"
 
-function Playlist({ playerIsHidden }) {
+function PlaylistInfo({ playerIsHidden }) {
 
   // global context
   const { token } = useContext(LoginStatusCtx)
@@ -30,15 +32,8 @@ function Playlist({ playerIsHidden }) {
   const [playlistDesc, setPlaylistDesc] = useState('')
   const [playlists, setPlaylists] = useState([])
   const [playlistArt, setPlaylistArt] = useState('')
-
-  // variables for drag and drop function
   const [draggables, setDraggables] = useState([])
-  let dragElIndex = 0
-  let dragElNewIndex = 0
-  let moveEl = null
-  let clone = null
   const container = useRef()
-  const playlist = useRef()
   // Update draggables array after elements are created using useCallback
   const setDraggableElement = useCallback(node => {
     if(node != null) {
@@ -46,10 +41,6 @@ function Playlist({ playerIsHidden }) {
       setDraggables(current => [...current, node])
     }
   },[])
-
-  const changeSong = (index) => {
-    changePlaylistSong(index, token, playerURIS)
-  } 
 
   const removeTrack = async (trackURI) => {
     removeTrackFromPlaylist(trackURI, token, playlistID)
@@ -65,10 +56,10 @@ function Playlist({ playerIsHidden }) {
     }, 2000)
   }
 
-  const changeOrder = async (dragElIndex, dragElNewIndex) => {
+  const changeOrder = async (startIndex, newIndex) => {
     setSongs([])
     setDraggables([])
-    changePlaylistOrder(dragElIndex, dragElNewIndex, token, playlistID)
+    changePlaylistOrder(startIndex, newIndex, token, playlistID)
       .then(result => {
         if(result.length > 0) return setSongs(result)
         console.error(result)
@@ -77,7 +68,11 @@ function Playlist({ playerIsHidden }) {
 
   const addToPlaylist = async (resultURI, playlistid) => {
     addTrackToPlaylist(resultURI, playlistid, token)
-      .then(result => console.error(result))
+    .then(result => {
+      if(result.length > 0) return
+      console.error(result)
+    })
+    document.querySelector('.show-p').classList.remove('show-p')
     setMessage('Song added to playlist')
     setShowMessage(true)
     // hide message after 2 seconds
@@ -86,113 +81,68 @@ function Playlist({ playerIsHidden }) {
     }, 2000)
   }
 
-  const showBtn = (e) => {
-    showHideAddToPlaylistBtn(e)
-  }
-
   useEffect(() => {
-    // get playlists for add to playlist functionality
-    const getPlaylists = async () => {
-      await axios.get('https://api.spotify.com/v1/me/playlists', {
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        }
-      }).then((res) => {
-        setPlaylists(res.data.items)
-      }).catch(error => console.error(error))
-    }
-    getPlaylists()
+    getUserPlaylists(token)
+    .then(result => {
+      if(result.length > 0) return setPlaylists(result)
+      console.error(result)
+    })
   },[token])
 
   useEffect(() => {
-    // when playlist changes, draggables array is emptied
-    // we don't want to run through all this code on empty array
-    // only run code when new draggables array is populated
+
     if(draggables.length === 0) return
 
-    // add event listeners for drag & drop functionality
     draggables.forEach(element => {
-      element.addEventListener('dragstart', drag)
+      element.addEventListener('dragstart', dragStart)
     })
 
-    function drag(e) {
-      dragElIndex = draggables.indexOf(e.target)
-      // create a clone which follows the mouse cursor
-      clone = e.target.cloneNode(true)
-      e.target.classList.add('move')
-      playlist.current.appendChild(clone)
+    function dragStart(e) {
+      console.log('dragstart')
+      let element = e.target
+      let startIndex = draggables.indexOf(element)
+      // create a copy of the dragging element for effect
+      let clone = element.cloneNode(true)
+      document.body.appendChild(clone)
       clone.classList.add('clone')
-      // set clones width to same as elements width (width varies with screen size)
-      clone.style.width = `${e.target.offsetWidth}px`
-      // adjust clone position if small device or large device
-      if(window.innerWidth < 900) {
-        clone.style.top = '50px'
-      } else {
-        clone.style.top = '-145px'
-      }
+      clone.style.height = `${element.offsetHeight}px`
+      clone.style.width = `${element.offsetWidth}px`
+      clone.style.position = 'absolute'
+      clone.style.top = '-55px'
       clone.style.left = `-${e.offsetX}px`
-      clone.style.setProperty('--x', e.clientX + 'px')
-      clone.style.setProperty('--y', e.clientY + 'px')
-      clone.classList.add('dragging')
-      // stop dragstart event listener to allow mousemove listener to take over
+      element.style.opacity = '0.5'
+      // cancel drag listener, start listening for mousemove instead
       e.preventDefault()
-      document.addEventListener('mousemove', mousePos)
-    }
-  
-    // on mousemove have clone follow the cursor
-    function mousePos(e) {
-      clone.style.setProperty('--x', e.clientX + 'px')
-      clone.style.setProperty('--y', e.clientY + 'px')
-      let nearestNode = getNearestNode(e.clientY)
-      moveEl = document.querySelector('.move')
-      // swap original element with nearest node
-      container.current.insertBefore(moveEl, nearestNode)
-      document.addEventListener('mouseup', placeClone)
-    }
-  
-    function getNearestNode(y) {
-      let nodes = [...container.current.querySelectorAll('.draggable:not(.clone)')]
-      return nodes.reduce((closest, child) => {
-        const box = child.getBoundingClientRect()
-        const offset = y - box.top - box.height / 2
-        if(offset < 0 && offset > closest.offset) {
-          return { offset:offset, element: child }
-        } else {
-          return closest
-        }
-      }, {offset: Number.NEGATIVE_INFINITY}).element
-    }
-  
-    // on mouseup delete the clone and place original element in its current position
-    function placeClone() {
-      if (document.querySelector('.move')) {
-        // get drag & dropped element reference
-        const tempEl = document.querySelector('.move')
-        document.querySelector('.move').classList.remove('move')
-        document.removeEventListener('mousemove', mousePos)
-        document.removeEventListener('mouseup', placeClone)
-        clone.remove()
-        // get nodelist from html and convert to array
-        // check converted array for drag & drop elements new index in playlist
-        const draggableNodeList = container.current.querySelectorAll('.draggable')
-        const convertedNodelist = Array.from(draggableNodeList)
-        dragElNewIndex = convertedNodelist.indexOf(tempEl)
-        // update draggables array with elements new index
-        draggables.splice(dragElNewIndex, 0, draggables.splice(dragElIndex, 1)[0]);
-        // if position is unchanged, dont send api request
-        if(dragElIndex === dragElNewIndex) return
-        // delay changes to prevent bugs
+
+      document.addEventListener('mousemove', mouseMove)
+      function mouseMove(e) {
+        clone.style.setProperty('--x', e.clientX + 'px')
+        clone.style.setProperty('--y', e.clientY + 'px')
+        let nearestNode = getNearestNode(e.clientY)
+        container.current.insertBefore(element, nearestNode)
+      }
+
+      document.addEventListener('mouseup', placeEl)
+      function placeEl() {
+        element.style.opacity = '1'
+        document.querySelector('.clone')?.remove()
+        document.removeEventListener('mousemove', mouseMove)
+        document.removeEventListener('mouseup', placeEl)
+        let newElLocation = container.current.querySelector(`[data-index="${startIndex}"]`)
+        // get new index of moved element
+        let htmlElToArray = Array.from(container.current.childNodes)
+        let newIndex = htmlElToArray.indexOf(newElLocation)
+        // Only send API request if element has moved positions
+        if(startIndex === newIndex) return
         setTimeout(() => {
-          changeOrder(dragElIndex, dragElNewIndex)
-        }, 500)
+          changeOrder(startIndex, newIndex)
+        }, 200)
       }
     }
     // cleanup event listeners on component re-render
     return () => {
       draggables.forEach(element => {
-        element.removeEventListener('dragstart', drag)
+        element.removeEventListener('dragstart', dragStart)
       })
     }
   },[draggables])
@@ -246,7 +196,7 @@ function Playlist({ playerIsHidden }) {
   },[playerCBData, setSongs, playlistID, token])
 
   return (
-    <div style={!playlistID ? {gridTemplateColumns:"unset"}:{}} className={playerIsHidden === true ? "playlist-wrap hide" : "playlist-wrap"} ref={playlist}>
+    <div style={!playlistID ? {gridTemplateColumns:"unset"}:{}} className={playerIsHidden === true ? "playlist-wrap hide" : "playlist-wrap"}>
       
       <CurrentSong currentSong={currentSong} />
 
@@ -273,9 +223,9 @@ function Playlist({ playerIsHidden }) {
           {
             playlistID? songs.map((song, index) => {
               return (
-                <span key={index} className={playerCBData.track_id === song.track.id ? "draggable selected" : "draggable"} draggable="true" ref={setDraggableElement}>
+                <span key={index} data-index={index} className={playerCBData.track_id === song.track.id ? "draggable selected" : "draggable"} draggable="true" ref={setDraggableElement}>
                   <span>{index+1}</span>
-                  <button onClick={() => changeSong(index)} className="play-song-btn">
+                  <button onClick={() => changePlaylistSong(index, token, playerURIS)} className="play-song-btn">
                   <img src={
                     song.track.album.images.length === 0 ?
                     'no image found' :
@@ -286,7 +236,7 @@ function Playlist({ playerIsHidden }) {
                     song.track.album.images.length === 0 ?
                     'no image found' :
                     `${song.track.name} album art`
-                    } draggable="true" />
+                    } draggable="false" />
                   </button>
                   <span className="play-song-tooltip">Play</span>
                   <span className="draggable-trackname">
@@ -300,7 +250,7 @@ function Playlist({ playerIsHidden }) {
                       <svg xmlns="http://www.w3.org/2000/svg" fill="currentcolor" width="10px" viewBox="0 0 320 400">{/* Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
                     </button>
                     :
-                    <button className="add-to-playlist" onClick={(e) => showBtn(e.target)}>
+                    <button className="add-to-playlist" onClick={(e) => showHideAddToPlaylistBtn(e.target)}>
                       <svg style={{pointerEvents:"none"}} xmlns="http://www.w3.org/2000/svg" fill="currentcolor" width="20px" viewBox="0 0 512 512">{/* Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg>
                       <span className="choose-playlist">
                         <h3>Add to playlist:</h3>
@@ -325,4 +275,4 @@ function Playlist({ playerIsHidden }) {
   ) 
 }
 
-export default Playlist
+export default PlaylistInfo
