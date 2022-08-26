eab1ff6500645c82f25e19275570e9a97d27e90c
adamdgit <adam_d@outlook.com>
2022-08-26 16:31:23 +1000

    fixed code indent spacing

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <BrowserRouter> 
-      <Routes>
-        <Route path="*" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+  <BrowserRouter> 
+    <Routes>
+      <Route path="*" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
 
 );
