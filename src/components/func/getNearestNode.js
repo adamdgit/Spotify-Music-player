2fc3c64ba661ce988f7447ae867cb507d17d1c6d
adamdgit <adam_d@outlook.com>
2022-08-26 16:35:52 +1000

    exported function to own component in func folder

---
+++ src/components/func/getNearestNode.js
@@ -0,0 +1,12 @@
+export function getNearestNode(y) {
+  let nodes = [...document.querySelectorAll('.draggable')]
+    return nodes.reduce((closest, child) => {
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2
+      if(offset < 0 && offset > closest.offset) {
+        return { offset:offset, element: child }
+      } else {
+        return closest
+      }
+    }, {offset: Number.NEGATIVE_INFINITY}).element
+}
