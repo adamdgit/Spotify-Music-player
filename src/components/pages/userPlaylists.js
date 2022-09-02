9081de332f4f7ed52a995c1bf6ceb893c0bdcb98
adamdgit <adam_d@outlok.com>
2022-09-02 17:35:46 +1000

    added div wrap around user-playlists results

--- src/components/pages/userPlaylists.js
+++ src/components/pages/userPlaylists.js
@@ -33,35 +33,37 @@ export default function UserPlaylists({...props}) {
   return (
     <div className="page-wrap">
       <div className="main-content">
-        <div className="create-playlists-wrap">
+        <div className="user-playlists-header">
           <h1>My Playlists</h1>
           <button className="play" onClick={() => createPlaylist(props.token, props.userID)}>Create Playlist</button>
         </div>
-        <>
-          {
-          playlists.length !== 0 ?
-          playlists.map((result, i) => {
-            return (
-              <div key={i} className={'my-playlists-result'}>
-                <img src={result.images.length !== 0 ? result.images[0].url : ''} alt={result.name + 'playlist art'} width={'200px'} height={'200px'} />
-                <h2>{result.name}</h2>
-                <p>{result.description}</p>
-                {
-                  result.owner.id !== userID 
-                  ? <button></button>
-                  : <NavLink to={`/editPlaylist/${result.id}`} className="edit-link">
-                      <button className="play edit">Edit</button>
-                    </NavLink>
-                }
-                <button className="play" onClick={() => playPlaylist(result)}>
-                  <svg viewBox="0 0 16 16" height="25" width="25" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
-                </button>
-              </div>
-            )
-          })
-          : <h1>No playlists found</h1>
-          }
-        </>
+        <div className="user-playlists-wrap">
+          <>
+            {
+            playlists.length !== 0 ?
+            playlists.map((result, i) => {
+              return (
+                <div key={i} className={'my-playlists-result'}>
+                  <img src={result.images.length !== 0 ? result.images[0].url : ''} alt={result.name + 'playlist art'} width={'200px'} height={'200px'} />
+                  <h2>{result.name}</h2>
+                  <p>{result.description}</p>
+                  {
+                    result.owner.id !== userID 
+                    ? <button></button>
+                    : <NavLink to={`/editPlaylist/${result.id}`} className="edit-link">
+                        <button className="play edit">Edit</button>
+                      </NavLink>
+                  }
+                  <button className="play" onClick={() => playPlaylist(result)}>
+                    <svg viewBox="0 0 16 16" height="25" width="25" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
+                  </button>
+                </div>
+              )
+            })
+            : <h1>No playlists found</h1>
+            }
+          </>
+        </div>
       </div>
     </div>
   )
