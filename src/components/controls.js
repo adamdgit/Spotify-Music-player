064de9bc92ccc23714c7bf14539aab7138df043a
adamdgit <adam_d@outlok.com>
2022-09-08 17:18:06 +1000

    Renamed global context + changed update msg to h3

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,14 +1,14 @@
 import { useState, useContext } from "react"
 import PlaylistInfo from "./playlistInfo"
-import { LoginStatusCtx } from "./login";
+import { GlobalContext } from "./login";
 import WebPlayback from "./func/webPlayBackSDK";
 
 function Controls() {
 
   // global context
-  const { token } = useContext(LoginStatusCtx)
-  const { message, setMessage } = useContext(LoginStatusCtx)
-  const { showMessage, setShowMessage } = useContext(LoginStatusCtx)
+  const { token } = useContext(GlobalContext)
+  const { message, setMessage } = useContext(GlobalContext)
+  const { showMessage, setShowMessage } = useContext(GlobalContext)
 
   const [playerIsHidden, setPLayerIsHidden] = useState(true)
 
@@ -21,7 +21,7 @@ function Controls() {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="20px" fill="currentcolor">{/*! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.*/}<path d="M9.39 265.4l127.1-128C143.6 131.1 151.8 128 160 128s16.38 3.125 22.63 9.375l127.1 128c9.156 9.156 11.9 22.91 6.943 34.88S300.9 320 287.1 320H32.01c-12.94 0-24.62-7.781-29.58-19.75S.2333 274.5 9.39 265.4z"/></svg>
       </button>
       <div className={showMessage === true ? "playlist-update-message show" : "playlist-update-message"}>
-        <h2>{message}</h2>
+        <h3>{message}</h3>
         <span className="triangle"></span>
       </div>
     </div>
