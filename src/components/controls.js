7ff5cf7d001b2f0e2d30935c3c461bafcd7ee141
adamdgit <adam_d@outlok.com>
2022-09-09 15:06:45 +1000

    Moved rotate to svg instead of parent button

--- src/components/controls.js
+++ src/components/controls.js
@@ -18,8 +18,8 @@ function Controls() {
     <PlaylistInfo playerIsHidden={playerIsHidden} />
     <div className="controls">
       <WebPlayback token={token}/>
-      <button className={playerIsHidden === true ? 'show-hide-playlist-btn' : 'show-hide-playlist-btn rotate-btn'} onClick={() => setPLayerIsHidden(!playerIsHidden)} >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="20px" fill="currentcolor">{/*! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.*/}<path d="M9.39 265.4l127.1-128C143.6 131.1 151.8 128 160 128s16.38 3.125 22.63 9.375l127.1 128c9.156 9.156 11.9 22.91 6.943 34.88S300.9 320 287.1 320H32.01c-12.94 0-24.62-7.781-29.58-19.75S.2333 274.5 9.39 265.4z"/></svg>
+      <button className={'show-hide-playlist-btn'} onClick={() => setPLayerIsHidden(!playerIsHidden)} >
+        <svg className={playerIsHidden === true ? '' : 'rotate-btn'} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" height="30px" width="30px" fill="currentcolor">{/*! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.*/}<path d="M9.39 265.4l127.1-128C143.6 131.1 151.8 128 160 128s16.38 3.125 22.63 9.375l127.1 128c9.156 9.156 11.9 22.91 6.943 34.88S300.9 320 287.1 320H32.01c-12.94 0-24.62-7.781-29.58-19.75S.2333 274.5 9.39 265.4z"/></svg>
       </button>
       <StatusMessage />
     </div>
