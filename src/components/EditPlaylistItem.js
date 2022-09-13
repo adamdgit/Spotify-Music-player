e8cac06778b863501965e7dfee9c7b964ebcf3ed
adamdgit <adam_d@outlok.com>
2022-09-13 18:22:13 +1000

    Removed unnecessary info from draggable element

--- src/components/EditPlaylistItem.js
+++ src/components/EditPlaylistItem.js
@@ -1,5 +1,4 @@
-import { convertTime } from "./func/convertTime"
-import { sanitizeArtistNames } from "./func/sanitizeArtistNames"
+import { sanitizeArtistNames } from "./utils/sanitizeArtistNames"
 
 export default function PlaylistItem({song, index, func, setDraggableElement}) {
 
@@ -17,14 +16,12 @@ export default function PlaylistItem({song, index, func, setDraggableElement}) {
         'no image found' :
         `${song.track.name} album art`
         } />
-      <span className="play-song-tooltip">Play</span>
       <span className="draggable-trackname">
         <h1>{song.track.name}</h1>
         <p>{sanitizeArtistNames(song.track.artists)}</p>
       </span>
-      <p className="song-length">{convertTime(song.track.duration_ms)}</p>
       <button className="remove-track-btn" title="remove track from playlist" onClick={() => func(song.track.uri)}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentcolor" width="10px" viewBox="0 0 320 400">{/* Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="15px" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
       </button>
       <button className="drag-icon" title="hold to move track position">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
