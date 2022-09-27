14261e1ec2f8009f695c20ce98d4720ddb5f8372
adamdgit <adam_d@outlook.com>
2022-09-27 23:42:35 +1000

    Now compares offset height of page-wrap to prevent bugs

--- src/components/utils/showHideAddToPlaylistBtn.js
+++ src/components/utils/showHideAddToPlaylistBtn.js
@@ -16,7 +16,7 @@ export function showHideAddToPlaylistBtn(e) {
   // if modal appears below the screen, change its position so user can see it
   // +100 is a magic number, accounting for padding and margins as javascript
   // does not have a built in way to get an elements height including those properties
-  if(e.querySelector('.choose-playlist').getBoundingClientRect().bottom > (document.querySelector('.playlist').offsetHeight + 100)) {
+  if(e.querySelector('.choose-playlist').getBoundingClientRect().bottom > (document.querySelector('.page-wrap').offsetHeight + 100)) {
     e.querySelector('.choose-playlist').style.top = '-355px'
   }
 }
