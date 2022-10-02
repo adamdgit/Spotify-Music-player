4b691692d13fe101ba070094359201886f1ccf49
adamdgit <adam_d@outlook.com>
2022-10-02 20:17:40 +1100

    Move featured playlists to playlists page

--- src/components/pages/explore.js
+++ src/components/pages/explore.js
@@ -10,26 +10,16 @@ export default function Explore() {
   const { setContextURI } = useContext(GlobalContext)
   const { setPlaylistID } = useContext(GlobalContext)
 
-  const [results, setResults] = useState([])
-  const trackElement = useRef([])
-
-  function playPlaylist(playlist){
-    // save currently playing playlist data to global context
-    setContextURI(playlist.uri)
-    setPlaylistID(playlist.id)
-    changePlaylistSong(0, token, playlist.uri)
-  }
- 
   useEffect(() => {
     const getFeaturedPlaylists = async () => {
-      await axios.get('https://api.spotify.com/v1/browse/featured-playlists', {
+      await axios.get('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
         headers: {
           Accept: 'application/json',
           Authorization: `Bearer ${token}`,
           'Content-Type': 'application/json',
         }
       }).then((res) => {
-        setResults(res.data.playlists.items)
+        console.log(res)
       }).catch(error => console.error(error))
     }
     getFeaturedPlaylists()
@@ -38,34 +28,7 @@ export default function Explore() {
   return (
     <div className="page-wrap">
       <div className="main-content">
-        <h1 className="featured">Spotify featured playlists:</h1>
-        <div className="explore-wrap">
-          {
-            results? results.map((result, i) => {
-              if (result === null || result === undefined) return
-              return (
-                <div key={i} ref={trackElement[i]} className={'explore-result'}>
-                  <img src={
-                    result.images.length === 0 ?
-                    'no image found' :
-                    result.images[0].url
-                    } alt={
-                    result.images.length === 0 ?
-                    'no image found' :
-                    `${result.name} playlist art`
-                    } width={'200px'} height={'200px'} />
-                  <h2>{result.name}</h2>
-                  <p>{result.description}</p>
-                  <button className="play" onClick={() => playPlaylist(result)}>
-                    <svg viewBox="0 0 16 16" height="20px" width="20px" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
-                  </button>
-                </div>
-              )
-            })
-            : results?.length === 0 ? <h1>No data found</h1>
-            : <Loading loadingMsg={'Fetching featured playlists...'}/>
-          }
-        </div>
+
       </div>
     </div>
   )
