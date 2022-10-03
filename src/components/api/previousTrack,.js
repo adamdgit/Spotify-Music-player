10a8d1c0dd2a8e95ff586e9c3fa59dd9c7a59ebd
adamdgit <adam_d@outlook.com>
2022-10-03 19:47:19 +1100

    removed irrelevant comment

--- src/components/api/previousTrack,.js
+++ src/components/api/previousTrack,.js
@@ -3,7 +3,7 @@ import axios from "axios"
 let error
 
 export async function previousTrack(token) {
-  // transfer playback to web player SDK
+
   await axios({ 
     method: 'post', 
     url: `https://api.spotify.com/v1/me/player/previous`, 
