d5fa3aa55435711b4bc35ed5ab8d67efc31b5def
adamdgit <adam_d@outlook.com>
2022-10-04 15:39:13 +1100

    Removed unused context

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom"
 
 function Header() {
 
-  const { token, setToken } = useContext(GlobalContext)
+  const { setToken } = useContext(GlobalContext)
   const { username } = useContext(GlobalContext)
 
   const logout = () => {
