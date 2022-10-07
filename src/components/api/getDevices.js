f0735c00503990fe8854fe41f594d140f424a71a
adamdgit <adam_d@outlook.com>
2022-10-07 19:44:33 +1100

    Get playback devices api call created

---
+++ src/components/api/getDevices.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+export async function getDevices(token) {
+
+  let devices = []
+  let errorMsg = false
+  
+  await axios.get(`https://api.spotify.com/v1/me/player/devices`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    }
+  }).then((res) => {
+    devices = res.data.devices
+  }).catch(error => errorMsg = error)
+
+  return {
+    errorMsg,
+    devices
+  }
+}
