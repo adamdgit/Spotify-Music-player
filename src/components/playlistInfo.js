65defd8f455f1105656111f2ef2fdbb50dffe317
adamdgit <adam_d@outlook.com>
2022-10-07 19:43:25 +1100

    Handle for no data to prevent crash

--- src/components/playlistInfo.js
+++ src/components/playlistInfo.js
@@ -180,7 +180,7 @@ function PlaylistInfo() {
       }
       getCurrentTrack()
       // get playlist data
-      if(contextURI.includes('playlist')) {
+      if(contextURI?.includes('playlist')) {
         const getPlaylistData = async () => {
           await axios.get(`https://api.spotify.com/v1/playlists/${contextID}?limit=50`, {
             headers: {
@@ -201,7 +201,7 @@ function PlaylistInfo() {
         getPlaylistData()
       }
       // get album data
-      if (contextURI.includes('album')) {
+      if (contextURI?.includes('album')) {
         const getAlbumData = async () => {
           await axios.get(`https://api.spotify.com/v1/albums/${contextID}`, {
             headers: {
@@ -251,7 +251,7 @@ function PlaylistInfo() {
         
         <div className="container" ref={container}>
           {
-            songs && contextURI.includes('playlist') ? songs.map((song, index) => {
+            songs && contextURI?.includes('playlist') ? songs.map((song, index) => {
               if (song === null || song === undefined) return null
               return (
                 <span key={index} data-index={index} className={currentTrackID === song.track.id ? "draggable selected" : "draggable"} draggable="true" ref={playlistOwner === userID ? setDraggableElement : setNull}>
@@ -280,7 +280,8 @@ function PlaylistInfo() {
                   <p className="song-length">{convertTime(song.track.duration_ms)}</p>
                   {
                     playlistOwner === userID ?
-                    <button className="remove-track-btn" title="remove track from playlist" onClick={() => removeTrack(song.track.uri)}>
+                    <button className="remove-track-btn" onClick={() => removeTrack(song.track.uri)}>
+                      <Tooltip tip={'Remove Track'} />
                       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="15px" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
                     </button>
                     :
@@ -305,7 +306,7 @@ function PlaylistInfo() {
           }
 
           {
-            songs && contextURI.includes('album') ? songs.map((song, index) => {
+            songs && contextURI?.includes('album') ? songs.map((song, index) => {
               if (song === null || song === undefined) return null
               return (
                 <span key={index} data-index={index} className={currentTrackID === song.id ? "draggable selected" : "draggable"}>
