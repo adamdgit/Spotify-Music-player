d7563f5e5a86425a55d37b71304ae676987e8588
adamdgit <adam_d@outlook.com>
2022-10-12 16:40:41 +1100

    Show default icon if no image data available

--- src/components/pages/library.js
+++ src/components/pages/library.js
@@ -109,11 +109,18 @@ export default function Library() {
             if (result === null || result === undefined) return <></>
             return (
               <div key={i} className={result.owner.id === userID ? 'result-large user' : 'result-large'}>
-                <img 
-                  src={result.images.length !== 0 ? result.images[0].url : ''} 
-                  alt={result.name + 'playlist art'} 
-                  onClick={() => playContext(result)}
-                />
+                {
+                  result.images.length === 0 ? 
+                  <span style={{display: 'grid', placeItems: 'center'}}>
+                    <svg role="img" fill="currentcolor" height="64" width="64" aria-hidden="true" viewBox="0 0 24 24"><path d="M6 3h15v15.167a3.5 3.5 0 11-3.5-3.5H19V5H8v13.167a3.5 3.5 0 11-3.5-3.5H6V3zm0 13.667H4.5a1.5 1.5 0 101.5 1.5v-1.5zm13 0h-1.5a1.5 1.5 0 101.5 1.5v-1.5z"></path></svg>
+                  </span>
+                  : 
+                  <img 
+                    src={result.images[0].url} 
+                    alt={result.name + 'playlist art'} 
+                    onClick={() => playContext(result)}
+                  />
+                }
                 <span className="result-info">
                   <h2>{result.name}</h2>
                   <p>{result.description}</p>
