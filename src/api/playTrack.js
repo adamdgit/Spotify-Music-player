a8a8601f9a8f850a023981d86210a4932968e76e
adamdgit <adam_d@outlook.com>
2022-10-14 04:21:06 +1100

    changed to pass uri only

--- src/api/playTrack.js
+++ src/api/playTrack.js
@@ -1,15 +1,15 @@
 import axios from "axios"
 
-export async function playTrack(token, song) {
+export async function playTrack(token, uri) {
 
   let errorMsg = false
-  // transfer playback to web player SDK
+
   await axios({ 
     method: 'put', 
     url: `https://api.spotify.com/v1/me/player/play`, 
     headers: { 'Authorization': 'Bearer ' + token }, 
     data: {
-      "uris": [song.uri],
+      "uris": [uri],
       "offset": { "position": 0 }
     }
   }).catch(err => errorMsg = err)
