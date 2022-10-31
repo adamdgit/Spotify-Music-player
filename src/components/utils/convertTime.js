a32a4d9b38c7f436215ad31fa5c27ca7ebeb57e0
adamdgit <adam_d@outlook.com>
2022-10-31 14:59:24 +1100

    Updated to support hours

--- src/components/utils/convertTime.js
+++ src/components/utils/convertTime.js
@@ -1,10 +1,17 @@
 // song duration given in MS, convert to mins + secs
 export function convertTime(millis) {
-  var minutes = Math.floor(millis / 60000);
-  var seconds = ((millis % 60000) / 1000).toFixed(0);
-  return (
-    seconds > 59 ?
-    (minutes+1) + ":00" :
-    minutes + ":" + (seconds < 10 ? "0" : "") + seconds
-  );
+  var seconds = (millis / 1000).toFixed(0);
+  var minutes = Math.floor(seconds / 60);
+  var hours = "";
+  if (minutes > 59) {
+      hours = Math.floor(minutes / 60);
+      minutes = minutes - (hours * 60);
+      minutes = (minutes >= 10) ? minutes : "0" + minutes;
+  }
+  seconds = Math.floor(seconds % 60);
+  seconds = (seconds >= 10) ? seconds : "0" + seconds;
+  if (hours != "") {
+      return hours + ":" + minutes + ":" + seconds;
+  }
+  return minutes + ":" + seconds;
 }
