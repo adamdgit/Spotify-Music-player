73bae44e68b721c6f111131b195316c0865f0914
adamdgit <adam_d@outlook.com>
2022-11-03 17:30:22 +1100

    songs are collected on click of artist

--- src/components/pages/explore.js
+++ src/components/pages/explore.js
@@ -17,10 +17,10 @@ export default function Explore() {
   const { contextID, setContextID } = useContext(GlobalContext)
   // playlist update message
   const { setMessage } = useContext(GlobalContext)
-
   const [userPlaylists, setUserPlaylists] = useState([])
   const [artists, setArtists] = useState([])
   const [topSongs, setTopSongs] = useState([])
+  const [selectedArtist, setSelectedArtist] = useState('')
 
   const addToPlaylist = (resultURI, playlistid, playlistName) => {
     addTrackToPlaylist(resultURI, playlistid, token)
@@ -48,6 +48,23 @@ export default function Explore() {
     setContextID('')
   }
 
+  const getArtistSongs = (id) => {
+    const getTopTracks = async (id) => {
+      await axios.get(`https://api.spotify.com/v1/artists/${id}/top-tracks?market=AU`, {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        }
+      }).then((res) => {
+        console.log(res.data)
+        setTopSongs(res.data.tracks)
+        document.querySelector('.page-wrap').scroll({top: 0, left: 0, behavior: 'smooth'})
+      }).catch(error => console.error(error))
+    }
+    getTopTracks(id)
+  }
+
   useEffect(() => {
 
     getUserPlaylists(token)
@@ -63,8 +80,8 @@ export default function Explore() {
 
   useEffect(() => {
 
-    const test = async () => {
-      await axios.get('https://api.spotify.com/v1/me/top/artists?limit=10', {
+    const getTopArtists = async () => {
+      await axios.get('https://api.spotify.com/v1/me/top/artists?limit=20', {
         headers: {
           Accept: 'application/json',
           Authorization: `Bearer ${token}`,
@@ -72,84 +89,65 @@ export default function Explore() {
         }
       }).then((res) => {
         setArtists(res.data.items)
+        console.log(res.data.items)
       }).catch(error => console.error(error))
     }
-    test()
+    getTopArtists()
 
   },[token, userID])
-
-  useEffect(() => {
-
-    if (artists.length > 0) {
-      const getTopTracks = async (id, name) => {
-        await axios.get(`https://api.spotify.com/v1/artists/${id}/top-tracks?market=AU`, {
-          headers: {
-            Accept: 'application/json',
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
-          }
-        }).then((res) => {
-          setTopSongs(current => 
-            [...current, {artistName: name,
-              artistID: id,
-              aritstTopTracks: [res.data.tracks] 
-            }]
-          )
-        }).catch(error => console.error(error))
-      }
-
-      artists.map(artist => {
-        getTopTracks(artist.id, artist.name)
-      })
-    }
-
-  },[token, artists])
 
   return (
     <div className="page-wrap">
       <div className="main-content">
-        <div className="artist-wrap">
-          <h1>Your top artists:</h1>
-          {
-            topSongs.length !== 0 ? topSongs.map((value, index) => {
+        <h1>Your top artists: {selectedArtist}</h1>
+        <div className="artist-top-songs">
+        {
+          topSongs.length !== 0 ? 
+            topSongs.map(track => {
               return (
-                <div key={value.artistID+index} className="artist">
-                  <h2>{value.artistName}</h2>
-                  <span className="artist-top-songs">
-                  {
-                    value.aritstTopTracks[0].map(track => {
-                      return (
-                        <div key={track.id} className="result-small">
-                          <img src={
-                          track.album.images.length === 0 ?
-                          'no image found' :
-                          track.album.images.length === 3 ?
-                          track.album.images[2].url :
-                          track.album.images[0].url
-                          } alt={
-                          track.album.images.length === 0 ?
-                          'no image found' :
-                          `${track.name} album art`
-                          } />
-                          <span>
-                            <h3>{track.name}</h3>
-                            <p>{sanitizeArtistNames(track.artists)}</p>
-                          </span>
-                          <AddToPlaylistBtn 
-                            track={track}
-                            userPlaylists={userPlaylists} 
-                            addToPlaylist={addToPlaylist} 
-                          />
-                          <button className="play" onClick={() => playSong(track.uri)}>
-                            <svg viewBox="0 0 16 16" height="20px" width="20px" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
-                          </button>
-                        </div>
-                      )
-                    })
-                  }
-                  </span>
-                </div>
+                <div key={track.id} className="result-small">
+                <img src={
+                track.album.images.length === 0 ?
+                'no image found' :
+                track.album.images.length === 3 ?
+                track.album.images[2].url :
+                track.album.images[0].url
+                } alt={
+                track.album.images.length === 0 ?
+                'no image found' :
+                `${track.name} album art`
+                } />
+                <span>
+                  <h3>{track.name}</h3>
+                  <p>{sanitizeArtistNames(track.artists)}</p>
+                </span>
+                <AddToPlaylistBtn 
+                  track={track}
+                  userPlaylists={userPlaylists} 
+                  addToPlaylist={addToPlaylist} 
+                />
+                <button className="play" onClick={() => playSong(track.uri)}>
+                  <svg viewBox="0 0 16 16" height="20px" width="20px" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
+                </button>
+              </div>
               )
+            })
+          : <></>
+        }
+        </div>
+        <div className="artist-wrap">
+          {artists.length !== 0 ?
+            artists.map(artist => {
+              return <span key={artist.id} className="result-artist" onClick={() => {
+                getArtistSongs(artist.id)
+                setSelectedArtist(artist.name)}
+                }>
+                <h2>{artist.name}</h2>
+                <img src={
+                  artist.images.length === 0 ? 'no image found'
+                  : artist.images[2].url} 
+                  alt={''} />
+              </span>
             })
             : <Loading loadingMsg={'Fetching your top artists...'}/>
           }
