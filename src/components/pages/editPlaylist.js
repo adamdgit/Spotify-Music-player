ee18377877d49cb256609c258d8a55e090dd071a
adamdgit <adam_d@outlook.com>
2022-11-09 00:04:16 +1100

    Fixed conditional rendering of placeholder track

--- src/components/pages/editPlaylist.js
+++ src/components/pages/editPlaylist.js
@@ -352,7 +352,8 @@ export default function EditPlaylist() {
         <h2 style={{textAlign: 'center', marginBottom: '2rem'}}>Edit tracks:</h2>
 
         <div className="edit-songlist" ref={container}>
-          {tracks.length === 0? 
+          {contextID === playlistData.id?
+            songs.length === 0 ? 
             <span className="edit-draggable">
               <span>{1}</span>
               <svg role="img" fill="currentcolor" height="32" width="32" aria-hidden="true" viewBox="0 0 24 24"><path d="M6 3h15v15.167a3.5 3.5 0 11-3.5-3.5H19V5H8v13.167a3.5 3.5 0 11-3.5-3.5H6V3zm0 13.667H4.5a1.5 1.5 0 101.5 1.5v-1.5zm13 0h-1.5a1.5 1.5 0 101.5 1.5v-1.5z"></path></svg>
@@ -370,9 +371,8 @@ export default function EditPlaylist() {
                   <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
                 </svg>
               </button>
-            </span> : <></>
-          }
-          {contextID === playlistData.id?
+            </span>
+            :
             songs.map((song, index) => {
               return (
                 <EditPlaylistItem 
@@ -385,6 +385,26 @@ export default function EditPlaylist() {
               )
             }) 
             : 
+            tracks.length === 0 ? 
+            <span className="edit-draggable">
+              <span>{1}</span>
+              <svg role="img" fill="currentcolor" height="32" width="32" aria-hidden="true" viewBox="0 0 24 24"><path d="M6 3h15v15.167a3.5 3.5 0 11-3.5-3.5H19V5H8v13.167a3.5 3.5 0 11-3.5-3.5H6V3zm0 13.667H4.5a1.5 1.5 0 101.5 1.5v-1.5zm13 0h-1.5a1.5 1.5 0 101.5 1.5v-1.5z"></path></svg>
+              <span className="draggable-trackname">
+                <h1>Song name..</h1>
+                <p>Artists..</p>
+              </span>
+              <button className="remove-track-btn" 
+                title="remove track from playlist"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="15px" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+              </button>
+              <button className="drag-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+                </svg>
+              </button>
+            </span> 
+            :
             tracks.map((song, index) => {
               return (
                 <EditPlaylistItem 
