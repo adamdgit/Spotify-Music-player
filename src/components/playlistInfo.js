e0e409f53b26ebbffbb1aa95c33ae5ed524eb21d
adamdgit <adam_d@outlook.com>
2022-11-20 18:19:01 +1100

    Edit playlist funcs removed from playlist info

--- src/components/playlistInfo.js
+++ src/components/playlistInfo.js
@@ -1,14 +1,11 @@
 import axios from "axios";
 import CurrentSong from "./currentSong";
-import { useState, useEffect, useRef, useContext, useCallback } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
 import { GlobalContext } from "./login";
 import { changePlaylistSong } from "../api/changePlaylistSong";
-import { removeTrackFromPlaylist } from "../api/removeTrackFromPlaylist"
-import { changePlaylistOrder } from "../api/changePlaylistOrder"
 import { addTrackToPlaylist } from "../api/addTrackToPlaylist"
 import { convertTime } from "./utils/convertTime"
 import { sanitizeArtistNames } from "./utils/sanitizeArtistNames"
-import { getNearestNode } from "./utils/getNearestNode"
 import { getUserPlaylists } from "../api/getUserPlaylists"
 import Tooltip from "./Tooltip";
 import AddToPlaylistBtn from "./AddToPlaylistBtn";
@@ -33,40 +30,6 @@ function PlaylistInfo() {
   const [playlistDesc, setPlaylistDesc] = useState('')
   const [playlists, setPlaylists] = useState([]) // filtered to show user owned
   const [playlistArt, setPlaylistArt] = useState('')
-  const [draggables, setDraggables] = useState([])
-  const container = useRef()
-  // Update draggables array after elements are created using useCallback
-  const setDraggableElement = useCallback(node => {
-    if(node != null) {
-      setDraggables(current => [...current, node])
-    }
-  },[])
-  const setNull = null
-
-  const removeTrack = (trackURI) => {
-    // empty songs array before re-populating with new data
-    setSongs([])
-    setDraggables([])
-
-    removeTrackFromPlaylist(trackURI, token, contextID)
-      .then(result => { 
-        if (result.errorMsg === false) return setSongs(result.tracks)
-        else console.error(result.errorMsg)
-      })
-    setMessage({msg: 'Song removed from playlist', needsUpdate: true})
-  }
-
-  const changeOrder = (startIndex, newIndex) => {
-    // empty songs array before re-populating with new data
-    setSongs([])
-    setDraggables([])
-
-    changePlaylistOrder(startIndex, newIndex, token, contextID)
-      .then(result => { 
-        if (result.errorMsg === false) return setSongs(result.tracks)
-        else console.error(result.errorMsg)
-      })
-  }
 
   const addToPlaylist = (resultURI, playlistid, playlistName) => {
     addTrackToPlaylist(resultURI, playlistid, token)
@@ -89,86 +52,9 @@ function PlaylistInfo() {
       })
   },[userID])
 
-  // Adds event listners for playlist items after the draggables array is filled
-  // useCallback provides update when html draggables are rendered
-  useEffect(() => {
-    if(draggables.length === 0) return
-
-    draggables.forEach(element => {
-      element.addEventListener('dragstart', dragStart)
-    })
-
-    function dragStart(e) {
-      let element = null
-      element = e.target
-      let startIndex = draggables.indexOf(element)
-      // create a copy of the dragging element for effect
-      let clone = element.cloneNode(true)
-      clone.style.setProperty('--x', e.clientX + 'px')
-      clone.style.setProperty('--y', e.clientY + 'px')
-      document.body.appendChild(clone)
-      clone.classList.add('clone')
-      clone.style.left = `-${e.offsetX}px`
-      clone.style.height = `${element.offsetHeight}px`
-      clone.style.width = `${element.offsetWidth}px`
-      clone.style.position = 'absolute'
-      clone.style.top = '-55px'
-      element.style.opacity = '0.3'
-      // cancel drag listener, start listening for mousemove instead
-      e.preventDefault()
-
-      document.addEventListener('pointermove', mouseMove)
-      function mouseMove(e) {
-        // scroll up or down if draggable element touches top or bottom of scroll area
-        let playlistEl = document.querySelector('.playlist')
-        if (document.querySelector('.container').offsetTop < playlistEl.scrollTop) {
-          if (e.clientY < 150) {
-            playlistEl.scrollTo({top: (playlistEl.scrollTop - 200), left: 0, behavior: 'smooth'})
-          }
-        }
-        if (e.clientY > playlistEl.offsetHeight + 100) { // +100 fpr header height
-          playlistEl.scrollTo({top: (playlistEl.scrollTop + 200), left: 0, behavior: 'smooth'})
-        }
-        // update draggable position
-        clone.style.setProperty('--x', e.clientX + 'px')
-        clone.style.setProperty('--y', e.clientY + 'px')
-        let nearestNode = getNearestNode(e.clientY, 'draggable')
-        // prevents constant rendering of element, only inserts when element is different
-        if (nearestNode !== element && nearestNode !== element.nextSibling) {
-          container.current.insertBefore(element, nearestNode)
-        }
-      }
-
-      document.addEventListener('pointerup', placeEl)
-      function placeEl() {
-        // remove listeners, place element, remove clone
-        element.style.opacity = '1'
-        document.querySelector('.clone')?.remove()
-        document.removeEventListener('pointermove', mouseMove)
-        document.removeEventListener('pointerup', placeEl)
-        let newElLocation = container.current.querySelector(`[data-index="${startIndex}"]`)
-        // get new index of moved element
-        let htmlElToArray = Array.from(container.current.childNodes)
-        let newIndex = htmlElToArray.indexOf(newElLocation)
-        // Only send API request if element has moved positions
-        if(startIndex === newIndex) return
-        setTimeout(() => {
-          changeOrder(startIndex, newIndex)
-        }, 200)
-      }
-    }
-    // cleanup event listeners on component re-render
-    return () => {
-      draggables.forEach(element => {
-        element.removeEventListener('dragstart', dragStart)
-      })
-    }
-  },[draggables])
-
   // cleanup arrays when playlist changes
   useEffect(() => {
     console.log('cleanup')
-    setDraggables([])
     setSongs([])
   },[contextURI, setSongs])
 
@@ -205,6 +91,7 @@ function PlaylistInfo() {
           }
         }).then((result) => {
           if (result.data) {
+            console.log(result)
             setPlaylistArt(result.data.images[0].url)
             setPlaylistDesc(result.data.description)
             setPlaylistName(result.data.name)
@@ -262,16 +149,14 @@ function PlaylistInfo() {
           <h3>Length</h3>
         </div>
         
-        <div className="container" ref={container}>
+        <div className="container">
           {
             contextURI?.includes('playlist') ? songs.map((song, index) => {
               if (song === null || song === undefined) return null
               return (
-                <span key={index} data-index={index} 
-                  style={playlistOwner === userID ? {cursor: 'move'} : {}}
-                  className={currentTrackID === song.track.id ? "draggable selected" : "draggable"} 
-                  draggable={playlistOwner === userID ? 'true' : 'false'} 
-                  ref={playlistOwner === userID ? setDraggableElement : setNull}>
+                <span key={index} data-index={index}
+                  style={playlistOwner === userID ? {gridTemplateColumns: '18px 80px auto 40px'} : {}} 
+                  className={currentTrackID === song.track.id ? "draggable selected" : "draggable"}> 
                   <span>{index+1}</span>
                   <button onClick={() => { changePlaylistSong(index, token, contextURI) }} className="play-song-btn">
                   <Tooltip tip={'Play'} />
@@ -294,10 +179,7 @@ function PlaylistInfo() {
                   <p className="song-length">{convertTime(song.track.duration_ms)}</p>
                   {
                     playlistOwner === userID ?
-                    <button className="remove-track-btn" onClick={() => removeTrack(song.track.uri)}>
-                      <Tooltip tip={'Remove Track'} />
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="15px" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
-                    </button>
+                    <></>
                     :
                     <AddToPlaylistBtn 
                       track={song.track}
