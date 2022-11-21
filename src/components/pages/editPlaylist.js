579bc91e8639fc1c30976e4dff6ea81762e1159f
adamdgit <adam_d@outlook.com>
2022-11-21 23:58:17 +1100

    Moved blank result on empty playlist to component

--- src/components/pages/editPlaylist.js
+++ src/components/pages/editPlaylist.js
@@ -9,6 +9,7 @@ import { getNearestNode } from "../utils/getNearestNode";
 import EditPlaylistItem from "../EditPlaylistItem";
 import Loading from "../Loading";
 import EditSearchResults from "../EditSearchResults";
+import BlankSongResult from "../BlankSongResult";
 
 export default function EditPlaylist() {
 
@@ -243,7 +244,6 @@ export default function EditPlaylist() {
       document.addEventListener('mousemove', mouseMove)
       document.addEventListener('touchmove', touchMove)
       function mouseMove(e) {
-        let nearestNode = null
         // scroll up or down if draggable element touches top or bottom of scroll area
         if (e.clientY < 200 || e.clientY > document.querySelector('.page-wrap').offsetHeight - 100 ) {
           cursorTouchingEdge(e)
@@ -252,7 +252,7 @@ export default function EditPlaylist() {
         }
         clone.style.setProperty('--x', e.clientX + 'px')
         clone.style.setProperty('--y', e.clientY + 'px')
-        nearestNode = getNearestNode(e.clientY, 'edit-draggable')
+        let nearestNode = getNearestNode(e.clientY, 'edit-draggable')
         // prevents constant rendering of element, only inserts when element is different
         if (nearestNode !== element && nearestNode !== element.nextSibling) {
           container.current.insertBefore(element, nearestNode)
@@ -260,7 +260,6 @@ export default function EditPlaylist() {
       }
 
       function touchMove(e) {
-        let nearestNode = null
         // scroll up or down if draggable element touches top or bottom of scroll area
         if (e.changedTouches[0].clientY < 200 || e.changedTouches[0].clientY > document.querySelector('.page-wrap').offsetHeight - 100 ) {
           cursorTouchingEdgeMobile(e)
@@ -268,7 +267,7 @@ export default function EditPlaylist() {
           clearTimeout(timer);
         }
         clone.style.setProperty('--y', e.changedTouches[0].clientY + 'px')
-        nearestNode = getNearestNode(e.changedTouches[0].clientY, 'edit-draggable')
+        let nearestNode = getNearestNode(e.changedTouches[0].clientY, 'edit-draggable')
         // prevents constant rendering of element, only inserts when element is different
         if (nearestNode !== element && nearestNode !== element.nextSibling) {
           container.current.insertBefore(element, nearestNode)
@@ -371,24 +370,7 @@ export default function EditPlaylist() {
         <div className="edit-songlist" ref={container}>
           {contextID === playlistData.id?
             songs.length === 0 ? 
-            <span className="edit-draggable">
-              <span>{1}</span>
-              <svg role="img" fill="currentcolor" height="32" width="32" aria-hidden="true" viewBox="0 0 24 24"><path d="M6 3h15v15.167a3.5 3.5 0 11-3.5-3.5H19V5H8v13.167a3.5 3.5 0 11-3.5-3.5H6V3zm0 13.667H4.5a1.5 1.5 0 101.5 1.5v-1.5zm13 0h-1.5a1.5 1.5 0 101.5 1.5v-1.5z"></path></svg>
-              <span className="draggable-trackname">
-                <h1>Song name..</h1>
-                <p>Artists..</p>
-              </span>
-              <button className="remove-track-btn" 
-                title="remove track from playlist"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="15px" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
-              </button>
-              <button className="drag-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                  <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
-                </svg>
-              </button>
-            </span>
+            <BlankSongResult />
             :
             songs.map((song, index) => {
               return (
@@ -403,24 +385,7 @@ export default function EditPlaylist() {
             }) 
             : 
             tracks.length === 0 ? 
-            <span className="edit-draggable">
-              <span>{1}</span>
-              <svg role="img" fill="currentcolor" height="32" width="32" aria-hidden="true" viewBox="0 0 24 24"><path d="M6 3h15v15.167a3.5 3.5 0 11-3.5-3.5H19V5H8v13.167a3.5 3.5 0 11-3.5-3.5H6V3zm0 13.667H4.5a1.5 1.5 0 101.5 1.5v-1.5zm13 0h-1.5a1.5 1.5 0 101.5 1.5v-1.5z"></path></svg>
-              <span className="draggable-trackname">
-                <h1>Song name..</h1>
-                <p>Artists..</p>
-              </span>
-              <button className="remove-track-btn" 
-                title="remove track from playlist"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="15px" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
-              </button>
-              <button className="drag-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                  <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
-                </svg>
-              </button>
-            </span> 
+            <BlankSongResult />
             :
             tracks.map((song, index) => {
               return (
