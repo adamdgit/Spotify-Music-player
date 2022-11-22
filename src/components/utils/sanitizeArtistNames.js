f20aad2ca245ea74c202d49b7704afb987962f9d
adamdgit <adam_d@outlook.com>
2022-11-22 16:15:43 +1100

    Fixed to handle commas with multiple artists

--- src/components/utils/sanitizeArtistNames.js
+++ src/components/utils/sanitizeArtistNames.js
@@ -1,8 +1,14 @@
 // takes in array of artist names and convert to a string
 export function sanitizeArtistNames(artists) {
-  let names = []
-  artists.map(artistName => {
-    return names.push(artistName.name)
+  let names = ''
+  artists.forEach((artistName, index) => {
+    if (index === 0 && index !== artists.length -1) {
+      return names += artistName.name + ', '
+    } else if (index === artists.length -1) {
+      return names += artistName.name
+    } else {
+      return names += artistName.name + ', '
+    }
   })
   return names.toString()
 }
