fd4ea226d8c375a13ebe2579c890e5afb1c0032f
adamdgit <adam_d@outlook.com>
2022-11-22 15:31:55 +1100

    Added total context duration state

--- src/components/playlistInfo.js
+++ src/components/playlistInfo.js
@@ -28,6 +28,7 @@ function PlaylistInfo() {
   const [playlistDesc, setPlaylistDesc] = useState('')
   const [playlists, setPlaylists] = useState([]) // filtered to show user owned
   const [playlistArt, setPlaylistArt] = useState('')
+  const [totalContextDuration, setContextDuration] = useState(0)
 
   const addToPlaylist = (resultURI, playlistid, playlistName) => {
     addTrackToPlaylist(resultURI, playlistid, token)
@@ -69,7 +70,6 @@ function PlaylistInfo() {
           }
         }).then((result) => {
           if (result.data) {
-            console.log(result)
             setPlaylistArt(result.data.images[0].url)
             setPlaylistDesc(result.data.description)
             setPlaylistName(result.data.name)
@@ -103,6 +103,14 @@ function PlaylistInfo() {
     }
   }, [contextURI])
 
+  useEffect(() => {
+    setContextDuration(0)
+    // get total playlist duration
+    songs.forEach(song => {
+      setContextDuration((current) => current += song.track.duration_ms)
+    })
+  }, [songs])
+
   return (
     <div style={!contextID ? {gridTemplateColumns:"unset"}:{}} className={playerIsHidden === true ? "playlist-wrap hide" : "playlist-wrap"}>
       
@@ -116,7 +124,11 @@ function PlaylistInfo() {
             />
           <span>
             <h1 style={{fontSize: '2.5rem'}}>{playlistName}</h1>
-            <h2>{playlistDesc}</h2>
+            <h2 style={{fontSize: '1rem', fontWeight: 'normal'}}>{playlistDesc}</h2>
+            <h3 style={{display: 'flex', gap: '1rem'}}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20px" fill="#999"><path d="M232 120C232 106.7 242.7 96 256 96C269.3 96 280 106.7 280 120V243.2L365.3 300C376.3 307.4 379.3 322.3 371.1 333.3C364.6 344.3 349.7 347.3 338.7 339.1L242.7 275.1C236 271.5 232 264 232 255.1L232 120zM256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48C141.1 48 48 141.1 48 256z"/></svg>
+              {convertTime(totalContextDuration)}
+            </h3>
           </span>
         </div>
 
