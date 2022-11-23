adb1177610b1ff2382928c9ab26df1fec9232870
adamdgit <adam_d@outlook.com>
2022-11-24 01:30:28 +1100

    Test - auto-refresh token on expire

--- src/components/login.js
+++ src/components/login.js
@@ -55,7 +55,12 @@ function Login() {
         setUserID(res.data.id)
         setUsername(res.data.display_name)
       }).catch(error => {
+        // we get 401 access token expired
+        // redirect -> endpoint
         console.error(error)
+        setToken('')
+        window.localStorage.removeItem('token')
+        window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
       })
     }
 
