1826ecdb677fd65e5a4b32ca65b8177584fffc28
adamdgit <adam_d@outlook.com>
2022-11-24 01:29:04 +1100

    Removed conditional render check

--- src/components/pages/artists.js
+++ src/components/pages/artists.js
@@ -106,37 +106,35 @@ export default function Explore() {
         <h1 style={{marginBottom: '2rem'}}>Your top artists: {selectedArtist}</h1>
         <div className="artist-top-songs">
         {
-          topSongs.length !== 0 ? 
-            topSongs.map(track => {
-              return (
-                <div key={track.id} className="result-small">
-                <img src={
-                  track.album.images.length === 0 ?
-                  'no image found' :
-                  track.album.images.length === 3 ?
-                  track.album.images[2].url :
-                  track.album.images[0].url
-                  } alt={
-                  track.album.images.length === 0 ?
-                  'no image found' :
-                  `${track.name} album art`
-                } />
-                <span className="info">
-                  <h3>{track.name}</h3>
-                  <p>{sanitizeArtistNames(track.artists)}</p>
-                </span>
-                <AddToPlaylistBtn 
-                  track={track}
-                  userPlaylists={userPlaylists} 
-                  addToPlaylist={addToPlaylist} 
-                />
-                <button className="play" onClick={() => playSong(track.uri)}>
-                  <svg viewBox="0 0 16 16" height="20px" width="20px" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
-                </button>
-              </div>
-              )
-            })
-          : <></>
+          topSongs.map(track => {
+            return (
+              <div key={track.id} className="result-small">
+              <img src={
+                track.album.images.length === 0 ?
+                'no image found' :
+                track.album.images.length === 3 ?
+                track.album.images[2].url :
+                track.album.images[0].url
+                } alt={
+                track.album.images.length === 0 ?
+                'no image found' :
+                `${track.name} album art`
+              } />
+              <span className="info">
+                <h3>{track.name}</h3>
+                <p>{sanitizeArtistNames(track.artists)}</p>
+              </span>
+              <AddToPlaylistBtn 
+                track={track}
+                userPlaylists={userPlaylists} 
+                addToPlaylist={addToPlaylist} 
+              />
+              <button className="play" onClick={() => playSong(track.uri)}>
+                <svg viewBox="0 0 16 16" height="20px" width="20px" fill="currentcolor"><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>
+              </button>
+            </div>
+            )
+          })
         }
         </div>
         <div className="artist-wrap">
