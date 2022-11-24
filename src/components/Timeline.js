88c5229d9299f16f39a03da8f60892ab0fc81b33
adamdgit <adam_d@outlook.com>
2022-11-25 04:22:09 +1100

    Timeline handles changing songs better

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,6 +3,7 @@ import { convertTime } from "./utils/convertTime"
 
 export default function Timeline({ ...props }) {
 
+
   const timelineSeek = (e) => {
     const rect = e.target.getBoundingClientRect()
     const percent = Math.min(Math.max(0, e.pageX - rect.x), rect.width) / rect.width
@@ -15,14 +16,19 @@ export default function Timeline({ ...props }) {
   useEffect(() => {
 
     if (props.is_paused === false) {
-      if (props.loading === true) props.setPos(0)
       const timelineUpdate = setInterval(() => {
         props.setPos(value => value +=1000)
       }, 1000)
+      // when track changes, loading = true from playback update
+      // we reset timer and clear interval to stop the timeline counting
+      if (props.loading === true) {
+        props.setPos(0)
+        clearInterval(timelineUpdate)
+      }
       return () => clearInterval(timelineUpdate)
     }
 
-  }, [props.is_paused])
+  }, [props.is_paused, props.loading])
 
   return (
     <div className="timeline-seek">
