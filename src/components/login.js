c59f53db8ffcc6dec25dae350b047ed5009a2c9c
adamdgit <adam_d@outlook.com>
2022-11-24 13:21:43 +1100

    removed auto-redirect when token expires

--- src/components/login.js
+++ src/components/login.js
@@ -60,7 +60,7 @@ function Login() {
         console.error(error)
         setToken('')
         window.localStorage.removeItem('token')
-        window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
+        //window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
       })
     }
 
