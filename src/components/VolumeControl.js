6b7653fc26620ffa89c30ac8d4622649aa33b6cc
adamdgit <adam_d@outlook.com>
2022-12-01 16:04:16 +1100

    moved isMuted boolean check to useEffect

--- src/components/VolumeControl.js
+++ src/components/VolumeControl.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import Tooltip from "./Tooltip";
 
 export default function VolumeControl({ player, volumeLS }) {
@@ -45,13 +45,13 @@ export default function VolumeControl({ player, volumeLS }) {
     }
   },[])
 
+  useEffect(() => {
+    isMuted === true ? player.setVolume(0) : player.setVolume(prevVolume)
+  }, [isMuted])
+
   return (
     <div className="volume-control">
-      <button className="volume-btn" onClick={() => {
-        setIsMuted(!isMuted)
-        // check opposite value as state will not update instantly
-        isMuted === false ? player.setVolume(0) : player.setVolume(prevVolume)
-      }}>
+      <button className="volume-btn" onClick={() => setIsMuted(!isMuted) }>
         <Tooltip tip={'Toggle mute'} />
         {
           isMuted === true ? <svg xmlns="http://www.w3.org/2000/svg" fill="currentcolor" width="20px" height="17px" viewBox="0 0 576 512"><path d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z"/></svg>
