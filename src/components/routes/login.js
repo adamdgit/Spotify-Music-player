3894aec835b53a71767b245727bde2e1808d9f55
adamdgit <adam_d@outlook.com>
2024-07-09 19:01:09 +1000

    bug fix, images can be null instead of empty array

--- src/components/routes/login.js
+++ src/components/routes/login.js
@@ -13,7 +13,7 @@ export const GlobalContext = React.createContext()
 function Login() {
 
   const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-  const REDIRECT_URI = "https://spotify-music-manager.vercel.app/spotify/"
+  const REDIRECT_URI = "http://localhost:3000/spotify/" //"https://spotify-music-manager.vercel.app/spotify/"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
   const SCOPE = 'user-library-read%20user-library-modify%20user-top-read%20user-read-private%20user-read-recently-played%20user-read-email%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-private%20playlist-modify-public%20playlist-read-private%20streaming'
